7a80355e365dc40b79ef61ef6ed4abef64d3e857
jan <janrb26@narancoinformatica.local>
2025-10-16 17:31:03 +0200

    conflicto solucionado

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -2,17 +2,12 @@ package ejercicios.depuracion;
 
 import java.util.Scanner;
 public class Ejemplo1While {
-    private int suma,contador;
-    public Ejemplo1While(){
-        this.suma=0;
-        this.contador=0;
-    }
-    =======
     private int suma,numero;
     public Ejemplo1While(){
         this.suma=0;
         this.numero=0;
     }
+    
     public static void main(String[] args) {
         int suma = 0, num;
         Scanner sc = new Scanner(System.in);
